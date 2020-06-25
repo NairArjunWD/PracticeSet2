f49c76803a1294ac0ec19dcc7aa1bcb54f584b28
Arjun Nair <nair.arjun47@yahoo.com>
2020-06-25 14:48:44 -0700

    practice codes

--- app/src/main/java/com/example/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/practiceset2/MainActivity.java
@@ -15,11 +15,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // PASTE CODE YOU WANT TO TEST HERE
-        int raspberryPrice = 5;
-        display1("1 box: $" + raspberryPrice);
-        raspberryPrice = 10;
-        display2("2 boxes: $" + (raspberryPrice));
-        display3("3 boxes: $" + (raspberryPrice * 3));
+//        int raspberryPrice = 5;
+//        display1("1 box: $" + raspberryPrice);
+//        raspberryPrice = 10;
+//        display2("2 boxes: $" + (raspberryPrice));
+//        display3("3 boxes: $" + (raspberryPrice * 3));
+
+//        int weekday = 5;
+//        int weekend = 9;
+//        int optimalHours = 7 * 8;
+//
+//        int actualHours = weekday;
+//        actualHours = actualHours + weekend * 2;
+//        int solution = optimalHours - actualHours;
+//        display(solution);
+
+//          int day1 = 15;
+//          int day2 = 22;
+//          int day3 = 18;
+//
+//          display(day1 + day2 + day3/
+
+//        int dollars = 40;
+//        int dollarsToYen = 119;
+//        int yen = dollarsToYen * dollars;
+//        display(yen);
+
+        String firstName = "Lyla";
+        String lastName = "Fujiwara";
+        String contactInfo = firstName + " " + lastName;
+        contactInfo = "<" + lastName + "." + firstName + "@justjava.com>";
+        display(contactInfo);
+
+
 
     }
 
